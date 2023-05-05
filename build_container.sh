47b2e0c97ba2e8f276ed3a512e99bd9593b2e113
Trevor Day <6400186-TrevorKMDay@users.noreply.gitlab.com>
2023-05-05 10:48:48 -0500

    add numpy to def

--- build_container.sh
+++ build_container.sh
@@ -2,10 +2,13 @@
 
 START=$(date '+%s')
 
+def=${1}
+sif="${def//.def/.sif}"
+
 # --force overwrites current version
 singularity build \
-    --fakeroot --fix-perms --force \
-    my_img.sif my_img.def
+    --fakeroot --fix-perms --force --writable-tmpfs \
+    "${sif}" "${def}"
 
 END=$(date '+%s')
 
@@ -15,4 +18,4 @@ min=$(echo "${sec} / 60" | bc -l)
 echo "Build time: ${sec} sec. ($(printf %.2f "${min}") min)"
 
 echo
-du -hsc my_img.sif
+du -hsc "${sif}"
